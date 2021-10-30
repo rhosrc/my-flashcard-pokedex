20df7d91ad0c78f004a423653041815af8bf8cf5
Chris Lafortune <chris.lafortune@gmail.com>
2021-10-30 18:46:59 -0400

    card back shows correct dex

--- js/script.js
+++ js/script.js
@@ -12,13 +12,8 @@ let speciesStats;
 let dexAPI;
 let dexEntry;
 let monArray = [];
+let dexArray = [];
 
-// class Cardcontainer {
-//     constructor(cardFront, cardBack) {
-//         this.cardFront = cardFront;
-//         this.cardBack = cardBack;
-//     }
-// }
 
 // Cached Element References
 
@@ -50,85 +45,75 @@ function handleSubmit(event) {
 function apiCall() {
     $.ajax(`${BASE_URL}?limit=151`)
         .then(function (monURL) {
+
             monAPI = monURL;
-            // console.log(monAPI);
-            monCall();
+
+            for (api of monAPI.results) {
+                $.ajax(api.url)
+                    .then(function (data) {
+                        // console.log(data);
+                        monArray.push(data);
+                    })
+            }
+
+            setTimeout(() => {
+                monArray.sort((a, b) => (a.id > b.id) ? 1 : -1);
+                // console.log('Monnnn', monArray);
+                monRender(monArray);
+            }, 2000);
+
+            for (api of monAPI.results) {
+                $.ajax(api.url)
+                    .then(function (data) {
+                        $.ajax(data.species.url)
+                            .then(function (dexInfo) {
+                                dexArray.push(dexInfo);
+                                // console.log(dexArray);
+                            })
+                    })
+
+            }
+
+            setTimeout(() => {
+                dexArray.sort((a, b) => (a.id > b.id) ? 1 : -1);
+                dexRender(dexArray);
+            }, 2000);   
         })
 }
 
 
 
-function monCall() {
-    for (let i = 0; i < monAPI.results.length; i++) {
-        $.ajax(`${monAPI.results[i].url}`)
-            .then(function (monObj) {
-                monStats = monObj;
-                // monArray.push(monStats);
-                // monArray = monStats.map(({id, name, weight, height, sprites}) => ({id, name, weight, height, sprites}));
-
-                $.ajax(monStats.species.url)
-                    .then(function (speciesData) {
-                        speciesStats = speciesData;
-                        console.log(speciesStats.flavor_text_entries[1].flavor_text);
-                    })
-
-
-                monRender();
-                // apiCall2();
-                // console.log(monStats);
-            })
-        // .then(monStats.species.url);
-    }
-
-}
-
-// function apiCall2() {
-//     $.ajax(`${BASE_URL}-species`)
-//         .then(function (dexURL) {
-//             dexAPI = dexURL;
-//             // console.log(dexAPI);
-//             dexCall();
-//             // console.log(monStats);
-//         })
-// }
-
-// function dexCall() {
-//     for (let i = 0; i < dexAPI.results.length; i++) {
-//         $.ajax(`${dexAPI.results[i].url}`)
-//             .then(function (dexObj) {
-//                 dexEntry = dexObj;
-//                 // console.log(dexEntry);
-//                 // console.log(dexEntry.flavor_text_entries[0].flavor_text);
-//                 dexRender();
-//             })
-//     }
-// }
-
-function monRender() {
-    $dex.append(
-        `<div class="pkmn-container">
-        <p>${monStats.id}</p>
-        <p><strong>${monStats.name.toUpperCase()}</strong></p><div class="container">
-        <div class="card"> 
-            <div class="front">
-            <img class="imeji" src="${monStats.sprites.other["official-artwork"].front_default}">
-            </div>
-            <div class="back">
-            <p class="entry">${speciesStats.flavor_text_entries[1].flavor_text}</p>
+function monRender(monarray) {
+    for (mon of monarray) {
+        $dex.append(
+            `<div class="pkmn-container">
+            <p>${mon.id}</p>
+            <p><strong>${mon.name.toUpperCase()}</strong></p><div class="container">
+            <div class="card"> 
+                <div class="front">
+                <img class="imeji" src="${mon.sprites.other["official-artwork"].front_default}">
+                </div>
+                <div class="back">
+                <p class="entry"></p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="details">
-    <p>Weighs ${Math.floor(monStats.weight / 4.536)} lbs</p>
-    <p>${Math.floor(monStats.height * 3.937)} inches tall</p>
-    </div>
-        </div> 
-        `
-    )
+        <div class="details">
+        <p>Weighs ${Math.floor(mon.weight / 4.536)} lbs</p>
+        <p>${Math.floor(mon.height * 3.937)} inches tall</p>
+        </div>
+            </div> 
+            `
+        )
+
+    }
 }
 
-// function dexRender() {
-//     $('.card .back').html(
-//         `<p class="entry">${dexEntry.flavor_text_entries[0].flavor_text}</p>`
-//     )
-// }
+function dexRender() {
+    // console.log(dexArray.length);
+    for (let i = 0; i < dexArray.length; i++) {
+        $('.entry')[i].append(
+            `${dexArray[i].flavor_text_entries[1].flavor_text}`
+        )
+    }
+}
