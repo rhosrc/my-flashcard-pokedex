b14498bc1d782631fb4e595d42f7166b8c641bdb
Chris Lafortune <chris.lafortune@gmail.com>
2021-10-31 20:42:54 -0400

    added dual types

--- js/script.js
+++ js/script.js
@@ -12,16 +12,97 @@ let limit;
 let offset;
 let imageURL;
 let dexEntry;
+let firstType;
+let firstColor;
+let secondColor;
+let secondType;
+let numOfTypes;
 
 
+
+let pokemonTypes = [{
+        name: 'bug',
+        hex: '#a8b820'
+    },
+    {
+        name: 'dark',
+        hex: '#705848'
+    },
+    {
+        name: 'dragon',
+        hex: '#7038f8'
+    },
+    {
+        name: 'electric',
+        hex: '#f8d030'
+    },
+    {
+        name: 'fairy',
+        hex: '#ee99ac'
+    },
+    {
+        name: 'fighting',
+        hex: '#c03028'
+    },
+    {
+        name: 'fire',
+        hex: '#f08030'
+    },
+    {
+        name: 'flying',
+        hex: '#a890f0'
+    },
+    {
+        name: 'ghost',
+        hex: '#705898'
+    },
+    {
+        name: 'grass',
+        hex: '#78c850'
+    },
+    {
+        name: 'ground',
+        hex: '#e0c068'
+    },
+    {
+        name: 'ice',
+        hex: '#98d8d8'
+    },
+    {
+        name: 'normal',
+        hex: '#a8a878'
+    },
+    {
+        name: 'poison',
+        hex: '#a040a0'
+    },
+    {
+        name: 'psychic',
+        hex: '#f85888'
+    },
+    {
+        name: 'rock',
+        hex: '#b8a038'
+    },
+    {
+        name: 'steel',
+        hex: '#b8b8d0'
+    },
+    {
+        name: 'water',
+        hex: '#6890f0'
+    },
+];
 
 // Cached Element References
 
 const $form = $('form');
 const $dex = $('#dex-section');
-const $card = $('.card-inner');
+const $card = $('.card .front');
 const $splash = $('.splash');
-const $top =  $('#page-up');
+const $top = $('#page-up');
+const $diagonal = $('.diagonal');
+
 
 // Event Listeners
 $form.on('submit', handleSubmit);
@@ -123,7 +204,8 @@ function apiCall() {
             for (api of monAPI.results) {
                 $.ajax(api.url)
                     .then(function (data) {
-                        // console.log(data);
+                        // console.log(data['types']);
+
                         monArray.push(data);
                     })
             }
@@ -131,6 +213,7 @@ function apiCall() {
             setTimeout(() => {
                 monArray.sort((a, b) => (a.id > b.id) ? 1 : -1);
                 // console.log('Monnnn', monArray);
+
                 monRender(monArray);
             }, 2000);
 
@@ -155,31 +238,55 @@ function apiCall() {
 }
 
 
-
 function monRender(monarray) {
     for (mon of monarray) {
+        firstType = pokemonTypes.find(obj => {
+            return obj.name === mon.types[0].type.name;
+        })
+        firstColor = firstType.hex;
+
+        for (let i = 0; i < mon.types.length; i++) {
+            numOfTypes = mon.types.length;
+            // console.log(numOfTypes);
+            if (numOfTypes === 2) {
+                secondType = mon.types[i].type;
+            } else if (numOfTypes === 1){
+                secondType = firstType;
+            }
+            searchOtherType = pokemonTypes.find(obj => {
+                return obj.name === secondType.name;
+            })
+        secondColor = searchOtherType.hex;
+        }
+
+
         $dex.append(
             `<div class="pkmn-container">
-            <p>${mon.id}</p>
-            <p><strong>${mon.name.toUpperCase()}</strong></p><div class="container">
-            <div class="card"> 
-                <div class="front">
-                <img class="imeji" src="${mon.sprites.other["official-artwork"].front_default}">
+                <p>${mon.id}</p>
+                <p><strong>${mon.name.toUpperCase()}</strong></p>
+                <div class="container">
+                    <div class="card">
+                    <div class="front" style="background-color: ${secondColor}">    
+                    <div class="diagonal" style="border-right-color: ${firstColor}"> 
+                            
+                            </div>    
+                            <img class="imeji" src="${mon.sprites.other["official-artwork"].front_default}">    
+                        </div>
+                        <div class="back">
+                            <p class="entry"></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="back">
-                <p class="entry"></p>
+                <div class="details">
+                    <p>Weighs ${Math.floor(mon.weight / 4.536)} lbs</p>
+                    <p>${Math.floor(mon.height * 3.937)} inches tall</p>
                 </div>
-            </div>
-        </div>
-        <div class="details">
-        <p>Weighs ${Math.floor(mon.weight / 4.536)} lbs</p>
-        <p>${Math.floor(mon.height * 3.937)} inches tall</p>
-        </div>
             </div> 
             `
         )
 
     }
+
 }
 
 function dexRender() {
