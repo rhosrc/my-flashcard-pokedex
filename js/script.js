3ced8c3793595034965d42cd23f0fd753e617e64
Chris Lafortune <chris.lafortune@gmail.com>
2022-02-21 02:07:46 -0500

    changed icon to pause

--- js/script.js
+++ js/script.js
@@ -221,7 +221,7 @@ function handleSubmit(event) {
             <img id="top" src=${imageURL}>
         <a/>
             <img id="play" src="https://img.icons8.com/ios/60/000000/play--v1.png"/>
-        <img id="pause" style="display: none" src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-mute-multimedia-kiranshastry-lineal-kiranshastry-1.png"
+        <img id="pause" style="display: none" src="https://img.icons8.com/ios/50/000000/pause--v1.png"
         </div>
         `
 
